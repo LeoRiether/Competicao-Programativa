933417318fecd53f88b984ffcc1b0c9ad7dac88b
Leonardo Riether <leonardoalves8217@gmail.com>
2021-01-07 14:31:44 -0200

    .

--- lib/String/SuffixAutomaton.cpp
+++ lib/String/SuffixAutomaton.cpp
@@ -77,35 +77,34 @@ void add(int c) {
     len[u] = len[last] + 1;
 
     int p = last;
-    for (; p > 0 && !to[p][c]; p = link[p]) {
+    last = u; // update last immediately
+    for (; p > 0 && !to[p][c]; p = link[p])
         to[p][c] = u;
-    }
 
     if (p == 0) {
         // this was the first time the character c was added to the string
         link[u] = 1;
-    } else {
-        // there's a transition from a substring to { substring c } (from p to
-        // q)
-        int q = to[p][c];
-        if (len[q] == len[p] + 1) {
-            // continuous transition
-            // we can just add u to the endpos-set of q and be fine
-            link[u] = q;
-        } else {
-            // non-continuous transition
-            // we need to split q into two distinct endpos-sets
-            int clone = ++lastID;
-            len[clone] = len[p] + 1;
-            link[clone] = link[q];
-            link[q] = link[u] = clone;
-            to[clone] = to[q];
-            for (int pp = p; to[pp][c] == q; pp = link[pp])
-                to[pp][c] = clone;
-        }
+        return;
     }
 
-    last = u;
+    // there's a transition from a substring to { substring c } (from p to q)
+    int q = to[p][c];
+    if (len[q] == len[p] + 1) {
+        // continuous transition
+        // we can just add u to the endpos-set of q and be fine
+        link[u] = q;
+        return;
+    }
+
+    // non-continuous transition
+    // we need to split q into two distinct endpos-sets
+    int clone = ++lastID;
+    len[clone] = len[p] + 1;
+    link[clone] = link[q];
+    link[q] = link[u] = clone;
+    to[clone] = to[q];
+    for (int pp = p; to[pp][c] == q; pp = link[pp])
+        to[pp][c] = clone;
 }
 
 void print_automaton(const vector<int> &nodes) {
