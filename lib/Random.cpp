24263872204e08732d8550bf84084c95191643a8
LeoRiether <leonardoalves8217@gmail.com>
2022-05-10 17:43:46 -0300

    mt19937_64

--- lib/Random.cpp
+++ lib/Random.cpp
@@ -1,2 +1,3 @@
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 uniform_int_distribution gen(a, b); // [a, b], inclusive
